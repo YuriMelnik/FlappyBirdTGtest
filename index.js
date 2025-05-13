c4f7d5a1ec25bacb64d904daa37f0256366e614f
YuriVM <73616856+YuriMelnik@users.noreply.github.com>
2025-05-13 22:17:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on("callback_query", function (query) {
         bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
     } else {
         queries[query.id] = query;
-        let gameurl = "https://github.com/YuriMelnik/FlappyBirdTGtest/index.js";
+        let gameurl = "https://github.com/YuriMelnik/FlappyBirdTGtest/";
         bot.answerCallbackQuery({
             callback_query_id: query.id,
             url: gameurl
